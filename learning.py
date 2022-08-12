9bf3bdebb5cd7c333e03f164c7b630315db9b1c9
NirTa <nir.taly@campus.technion.ac.il>
2022-08-13 00:46:52 +0300

    - validation, train, test update - "soft" handling

--- learning.py
+++ learning.py
@@ -1,16 +1,41 @@
+import os
+
 import simulator as sym
 import numpy as np
 import random
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 import pandas as pd
+import multiprocessing as mp
 
-action_dict = {0 :'H', 1 : 'S', 2 : 'P', 3 : 'D', 4 : 'X'}
+action_dict = {0: 'H', 1: 'S', 2: 'P', 3: 'D', 4: 'X'}
+epsilon = 0.3
+
+def run_loop(agent):
+    game_state, player_state, dealer_state = agent.Game.reset_hands()
+    done = False
+    while not done:
+        if random.uniform(0, 1) < epsilon:
+            # Random Action
+            # action = {0 :'H', 1 : 'S', 2 : 'P', 3 : 'D', 4 : 'X'}
+            action = int(random.randint(0, 3))
+        else:
+            action = agent.get_action(game_state, player_state, dealer_state)
+
+        next_game_state, next_player_state, next_dealer_state, reward, done = agent.Game.step(
+            action_dict[action])
+
+        agent.update_parameters(game_state, player_state, dealer_state, action, reward, next_game_state,
+                                next_player_state, next_dealer_state)
+
+        game_state = next_game_state
+        player_state = next_player_state
+        dealer_state = next_dealer_state
+    return reward
 
 class QAgent:
 
     def __init__(self, alpha, gamma):
-        #The BJ Simulator
+        # The BJ Simulator
         self.Game = sym.Game()
 
         #Init the table with q_values: HARD(18X10) X SOFT(9X10) X SPLIT(8X10) X ACTION(5)
@@ -18,130 +43,125 @@ class QAgent:
         self.Q_table = np.zeros((2,33,14,4)) #33 for all sums that are reachable, 14, for 1-13
         # self.Q_table[:,:,:,0:5] = 3
         self.alpha = alpha
-
         self.gamma = gamma
+        self.epislon = 0.3
 
     def get_action(self, game_state, player_state, dealer_state):
         return np.argmax(self.Q_table[int(game_state), player_state, dealer_state])
-    
 
-    def update_paramaters(self, game_state, player_state , dealer_state , action, reward, next_game_state,next_player_state,next_dealer_state):
-        
-        #Q-Learn formula
+    def update_parameters(self, game_state, player_state, dealer_state, action, reward, next_game_state,
+                          next_player_state, next_dealer_state):
+        # Q-Learn formula
         old_value = self.Q_table[int(game_state), player_state, dealer_state, action]
-        next_max = np.max(self.Q_table[int(next_game_state) ,next_player_state, next_dealer_state])
+        next_max = np.max(self.Q_table[int(next_game_state), next_player_state, next_dealer_state])
         new_value = old_value + self.alpha * (reward + self.gamma * next_max - old_value)
-        
-        #Update the Q_table
+
+        # Update the Q_table
         self.Q_table[int(game_state), player_state, dealer_state, action] = new_value
 
+    def train(self, n_train):
+        for _ in tqdm(range(0, n_train)):
+            # Reset the game to random state
+            run_loop(self)
+
+    def test(self, n_test):
+        wins = 0
+        for _ in tqdm(range(0, n_test)):
+            # Reset the game to random state
+            game_state, player_state, dealer_state = self.Game.reset_hands()
+            reward = 0
+            done = False
+            while not done:
+                # action = {0 :'H', 1 : 'S', 2 : 'P', 3 : 'D', 4 : 'X'}
+                action = self.get_action(game_state, player_state, dealer_state)
+                next_game_state, next_player_state, next_dealer_state, reward, done = self.Game.step(
+                    action_dict[action])
+                self.update_parameters(game_state, player_state, dealer_state, action, reward, next_game_state,
+                                       next_player_state, next_dealer_state)
+
+                game_state = next_game_state
+                player_state = next_player_state
+                dealer_state = next_dealer_state
+
+            if reward > 0:
+                wins += reward
+        return wins
+
+def validation(alpha):
+    """
+    best gamma = 0.001
+    best alpha = 0.08
+    """
+    n_learning = 5000
+    gammas = np.arange(0.001, 1, 0.005)
+
+    best_gamma = gammas[0]
+    best_wins = 0
+    for gamma in gammas:
+        agent = QAgent(alpha, gamma)
+        wins = 0
+        for _ in tqdm(range(0, n_learning)):
+            reward = run_loop(agent)
+
+            wins += reward > 0
+        if wins > best_wins:
+            best_gamma = gamma
+            best_wins = wins
+
+    return best_wins, best_gamma, alpha
+
 def main():
-    ''' Rewards:
+    """ Rewards:
             Regular WIN     = 1
             DoubleDown WIN  = 2
             SPLIT WIN       = 1 (per win)
             Lose            = -1
             SPLIT Lose      = 1 (per win)
             Surrender Lose  = -0.5
-    '''
-
-    '''
         States:
-            (0 = Hard / 1 = Soft / 2 = Splitable , HandSum, DealerSum)
-    '''
-    n_learning = 200000
+            (0 = Hard / 1 = Soft / 2 = Splittable , HandSum, DealerSum)
+    """
+    n_train = 100000
     n_test = 100000
-    alphas = [0.001]
-    gammas = [0.75]
-    epsilon = 0.6
+    alphas = np.arange(0.0001, 2, 0.005)
 
-    
-    
-    scatterx = []
-    scattery = []
-    scatterz = []
+    mp_pool = mp.Pool(os.cpu_count())
+    results = mp_pool.imap_unordered(validation, alphas)
+    best_rewards, best_gamma, best_alpha = validation(0.3)
 
-    for alpha in alphas:
-        for gamma in gammas:
-            agent = QAgent(alpha, gamma)
-            wins = 0
-            #Learning
-            for i in tqdm(range(0,n_learning)):
-                #Reset the game to random state
-                game_state, player_state, dealer_state = agent.Game.reset_hands()
-                reward = 0
-                done = False
-                epsilon *= 0.99999
-                while (not done):
-                    # action = {3 :'H', 1 : 'S', 2 : 'P', 0 : 'D', 4 : 'X'}
-                    if(random.uniform(0, 1) < epsilon):
-                        #Random Action
-                        action = int(random.randint(0, 3))
-                    else:
-                        action = agent.get_action(game_state,player_state,dealer_state)
+    best_result = max(results)
+    print (best_result)
+    best_gamma = best_result[1]
+    best_alpha = best_result[2]
+    # best_gamma = 0.001
+    # best_alpha = 0.08
+    # Training policy
+    agent = QAgent(best_alpha, best_gamma)
+    agent.train(n_train)
+    wins = agent.test(n_test)
 
-                    next_game_state, next_player_state, next_dealer_state, reward, done = agent.Game.step(action_dict[action])
-
-                    agent.update_paramaters(game_state, player_state, dealer_state ,action,reward,next_game_state,next_player_state,next_dealer_state)
-
-                    game_state = next_game_state
-                    player_state = next_player_state
-                    dealer_state = next_dealer_state
-                
-                # if(reward > 0):
-                #     wins+= reward
-                # print(wins/(i+1))
-
-            # #Testing The Policy
-            for i in tqdm(range(0,n_test)):
-                #Reset the game to random state
-                game_state, player_state, dealer_state = agent.Game.reset_hands()
-                reward = 0
-                done = False
-                while (not done):
-                    # action = {0 :'H', 1 : 'S', 2 : 'P', 3 : 'D', 4 : 'X'}
-                    action = agent.get_action(game_state,player_state,dealer_state)
-
-                    next_game_state, next_player_state, next_dealer_state, reward, done = agent.Game.step(action_dict[action])
-
-                    # agent.update_paramaters(game_state, player_state, dealer_state ,action,reward,next_game_state,next_player_state,next_dealer_state)
-
-                    game_state = next_game_state
-                    player_state = next_player_state
-                    dealer_state = next_dealer_state
-                
-                if(reward > 0):
-                    wins+= reward
-            # scatterx.append(alpha)
-            # scattery.append(epsilon)
-            # scatterz.append(wins/n_test)
-                
-    # fig = plt.figure(figsize=(5,8))
-    # ax = fig.add_subplot()
-    # ax.scatter(scattery, scatterz)
-    # plt.show()
-
-    hard_policy = np.argmax(agent.Q_table[0],axis=2).astype(str)
-    hard_policy[hard_policy=='0'] = 'H'
-    hard_policy[hard_policy=='1'] = 'S'
-    hard_policy[hard_policy=='2'] = 'P'
-    hard_policy[hard_policy=='3'] = 'D'
-    # hard_policy[hard_policy=='4'] = 'X'
-    hard_policy = hard_policy[4:22,1:]
-    print(pd.DataFrame(hard_policy,columns=['A',2,3,4,5,6,7,8,9,10,'J','Q','K'],index=list(range(4,22))))
+    hard_policy = np.argmax(agent.Q_table[0], axis=2).astype(str)
+    hard_policy[hard_policy == '0'] = 'H'
+    hard_policy[hard_policy == '1'] = 'S'
+    hard_policy[hard_policy == '2'] = 'P'
+    hard_policy[hard_policy == '3'] = 'D'
+    hard_policy[hard_policy == '4'] = 'X'
+    hard_policy = hard_policy[4:22, 1:]
+    print(pd.DataFrame(hard_policy, columns=['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'], index=list(range(4, 22))))
 
     print()
-    soft_policy = np.argmax(agent.Q_table[1],axis=2).astype(str)
-    soft_policy[soft_policy=='0'] = 'H'
-    soft_policy[soft_policy=='1'] = 'S'
-    soft_policy[soft_policy=='2'] = 'P'
-    soft_policy[soft_policy=='3'] = 'D'
-    # soft_policy[soft_policy=='4'] = 'X'
-    soft_policy = soft_policy[13:22,1:]
-    print(pd.DataFrame(soft_policy,columns=['A',2,3,4,5,6,7,8,9,10,'J','Q','K'],index=list(range(13,22))))
+    soft_policy = np.argmax(agent.Q_table[1], axis=2).astype(str)
+    soft_policy[soft_policy == '0'] = 'H'
+    soft_policy[soft_policy == '1'] = 'S'
+    soft_policy[soft_policy == '2'] = 'P'
+    soft_policy[soft_policy == '3'] = 'D'
+    soft_policy[soft_policy == '4'] = 'X'
+    soft_policy = soft_policy[13:22, 1:]
+    print(
+        pd.DataFrame(soft_policy, columns=['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'], index=list(range(13, 22))))
 
-    print((wins)/(n_test))
+    print(wins / n_test)
+
 
 if __name__ == '__main__':
-	main()
-
+    main()
--- simulator.py
+++ simulator.py
@@ -1,296 +1,267 @@
 import random
 from builtins import print
-from telnetlib import Telnet
 
-cards_dict={1: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10", 11: "J", 12: "Q", 13: "K"}
-cards_values={1: 11, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 10, 12: 10, 13: 10}
+cards_dict = {1: "A", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "10", 11: "J", 12: "Q",
+              13: "K"}
+cards_values = {1: 11, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 10, 12: 10, 13: 10}
 
 
 class Shoe:
-	def __init__(self, n=8):
-		self.n = n
-		self.cards = []
-		self.rebuild()
-		
-	def draw_card(self):
-		if not self.cards:
-			self.rebuild()
+    def __init__(self, n=8):
+        self.n = n
+        self.cards = []
+        self.rebuild()
 
-		return self.cards.pop(0)
+    def draw_card(self):
+        if not self.cards:
+            self.rebuild()
 
-	def rebuild(self):
-		self.cards = [*range(1, 14)] * 4 * self.n
-		random.shuffle(self.cards)
+        return self.cards.pop(0)
+
+    def rebuild(self):
+        self.cards = [*range(1, 14)] * 4 * self.n
+        random.shuffle(self.cards)
 
 
 class Game:
-	def __init__(self, money=100):
-		self.money = money
-		self.shoe = Shoe()
-		self.reset_hands()
-		# self.playerCards = [[]]
-		# self.dealerCards = []
-		# self.isSplit = False
-		# self.isDouble = False
+    def __init__(self, money=100):
+        self.money = money
+        self.shoe = Shoe()
+        self.reset_hands()
 
-	def reset_hands(self):
-		self.playerCards = [[]]
-		self.dealerCards = []
-		self.isSplit = False
-		self.isDouble = False
-		self.splitHand = 0
-		self.first_move = True
+    def reset_hands(self):
+        self.playerCards = [[]]
+        self.dealerCards = []
+        self.isSplit = False
+        self.isDouble = False
+        self.currHand = 0
+        self.first_move = True
 
-		#card draw
-		for j in range(0, 2):
-			self.playerCards[0].append(self.shoe.draw_card())
-			self.dealerCards.append(self.shoe.draw_card())
+        # card draw
+        for j in range(0, 2):
+            self.playerCards[0].append(self.shoe.draw_card())
+            self.dealerCards.append(self.shoe.draw_card())
 
-		game_state = 0
-		if 1 in self.playerCards[0]:
-			game_state = 1
-		player_state, _ = self.sum_hands(self.playerCards[0])
-		dealer_state = self.dealerCards[0]
-		return game_state, player_state, dealer_state
-		
+        game_state = 0
+        if 1 in self.playerCards[0]:
+            game_state = 1
+        player_state, _ = self.sum_hands(self.playerCards[0])
+        dealer_state = self.dealerCards[0]
+        return game_state, player_state, dealer_state
 
-	def print_hands(self, showDealerCard = True):
-		dealer_str = "Dealer's Cards: "
-		player_str = "Your Cards:     "
-		if not showDealerCard:
-			dealer_str += "* "
-		else:
-			dealer_str += cards_dict[self.dealerCards[0]]
-			dealer_str += " "
-		for card in self.dealerCards[1:]:
-			dealer_str += cards_dict[card]
-			dealer_str += " "
+    def print_hands(self, showDealerCard=True):
+        dealer_str = "Dealer's Cards: "
+        player_str = "Your Cards:     "
+        if not showDealerCard:
+            dealer_str += "* "
+        else:
+            dealer_str += cards_dict[self.dealerCards[0]]
+            dealer_str += " "
+        for card in self.dealerCards[1:]:
+            dealer_str += cards_dict[card]
+            dealer_str += " "
 
-		if showDealerCard:
-			print(dealer_str + "\t\t\t\tsum = " + str(self.sum_hands(self.dealerCards)))
-		else:
-			print(dealer_str + "\t\t\t\tsum = " + str(cards_values[self.dealerCards[1]]))
+        if showDealerCard:
+            print(dealer_str + "\t\t\t\tsum = " + str(self.sum_hands(self.dealerCards)))
+        else:
+            print(dealer_str + "\t\t\t\tsum = " + str(cards_values[self.dealerCards[1]]))
 
-		for i, hand in enumerate(self.playerCards):
-			if len(self.playerCards) > 1:
-				player_str = "Hand " + str(i) + ": "
-			for card in hand:
-				player_str += cards_dict[card]
-				player_str += " "
-			player_str += "\t\t\t\tsum = " + str(self.sum_hands(hand))
-			print(player_str)
-			print("")
+        for i, hand in enumerate(self.playerCards):
+            if len(self.playerCards) > 1:
+                player_str = "Hand " + str(i) + ": "
+            for card in hand:
+                player_str += cards_dict[card]
+                player_str += " "
+            player_str += "\t\t\t\tsum = " + str(self.sum_hands(hand))
+            print(player_str)
+            print("")
 
+    def sum_hands(self, hand):
+        hand_sum = sum(cards_values[i] for i in hand)
+        soft = (hand.count(1) == 1)
 
-	def sum_hands(self, hand):
-		hand_sum = sum(cards_values[i] for i in hand)
-		soft = False
+        for i in range(hand.count(1)):
+            if hand_sum > 21:
+                hand_sum -= 10
+        return hand_sum, soft
 
-		if 1 in hand:
-			soft = True
+    def manageBet(self, dealer_sum, player_sums, bet) -> int:
+        retval = 0
+        for i, _ in enumerate(self.playerCards):
+            if dealer_sum == player_sums[i]:
+                self.money += bet[i]
+            elif dealer_sum > 21:
+                self.money += 2 * bet[i]
+                retval += 1 + self.isDouble
+            elif dealer_sum < player_sums[i] <= 21:
+                self.money += 2 * bet[i]
+                retval += 1 + self.isDouble
+            else:
+                retval -= 1 + self.isDouble
 
-		for i in range(hand.count(1)):
-			#Ace is soft?
-			if hand_sum > 21:
-				hand_sum -= 10
-		return hand_sum, soft
+        return retval
 
-	def manageBet(self, dealer_sum, player_sums, bet) -> int:
-		retval = 0
-		for i,_ in enumerate(self.playerCards):
-			if dealer_sum == player_sums[i]:
-				self.money += bet[i]
-			elif dealer_sum > 21:
-				self.money += 2*bet[i]
-				retval += 1 + self.isDouble
-			elif player_sums[i] > dealer_sum and player_sums[i] <= 21:
-				self.money += 2*bet[i]
-				retval += 1 + self.isDouble
-			else:
-				retval -= 1 + self.isDouble
+    def rewardHandler(self, dealer_sum, player_sums) -> int:
+        reward = 0
+        for i, _ in enumerate(self.playerCards):
+            if dealer_sum > 21:
+                reward += 1 + self.isDouble
+            elif dealer_sum < player_sums[i] <= 21:
+                reward += 1 + self.isDouble
+            else:
+                reward -= 1 + self.isDouble
 
-		return retval
+        return reward
 
-	def rewardHandler(self, dealer_sum, player_sums) -> int:
-		reward = 0
-		for i,_ in enumerate(self.playerCards):
-			if dealer_sum > 21:
-				reward += 1 + self.isDouble
-			elif player_sums[i] > dealer_sum and player_sums[i] <= 21:
-				reward += 1 + self.isDouble
-			else:
-				reward -= 1 + self.isDouble
+    def run_game(self):
+        self.reset_hands()
 
-		return reward
+        # place bet
+        bet = [float(input("please place a bet: "))]
+        self.money -= bet[0]
 
-	def run_game(self):
-		self.reset_hands()
+        self.print_hands(False)
 
-		#place bet
-		bet = [float(input("please place a bet: "))]
-		self.money -= bet[0]
+        # check BJ
+        if sum(cards_values[i] for i in self.playerCards[0]) == 21:
+            self.money += 2.5 * bet[0]
+            return 1
+        if sum(cards_values[i] for i in self.dealerCards) == 21:
+            return -1
 
-		self.print_hands(False)
+        # Player Moves
+        first_move = True
+        for hand in self.playerCards:
+            while True:
+                if first_move:
+                    if hand[0] == hand[1]:
+                        move = input("Select your move: Hit - H, Stand - S, Double - D, Split - P, Surrender - X\n")
+                    else:
+                        move = input("Select your move: Hit - H, Stand - S, Double - D, Surrender - X\n")
+                else:
+                    move = input("Select your move: Hit - H, Stand - S\n")
+                move = move.upper()
 
-		#check BJ
-		if sum(cards_values[i] for i in self.playerCards[0]) == 21:
-			self.money += 2.5*bet[0]
-			return 1
-		if sum(cards_values[i] for i in self.dealerCards) == 21:
-			return -1
+                if move == "H":
+                    hand.append(self.shoe.draw_card())
+                    first_move = False
+                    if self.isSplit and cards_dict[hand[0]] == "A":
+                        break
+                elif move == "S":
+                    break
+                elif move == "D" and first_move:
+                    self.isDouble = True
+                    self.money -= bet[0]
+                    bet[0] *= 2
 
-		#Player Moves
-		first_move = True
-		move = ""
-		for hand in self.playerCards:
-			while True:
-				if first_move:
-					if hand[0] == hand[1]:
-						move = input("Select your move: Hit - H, Stand - S, Double - D, Split - P, Surrender - X\n")
-					else:
-						move = input("Select your move: Hit - H, Stand - S, Double - D, Surrender - X\n")
-				else:
-					move = input("Select your move: Hit - H, Stand - S\n")
-				move= move.upper()
+                    hand.append(self.shoe.draw_card())
+                    break
+                elif move == "P" and first_move and hand[0] == hand[1]:
+                    self.isSplit = True
+                    self.money -= bet[0]
+                    bet += bet
 
-				if move == "H":
-					hand.append(self.shoe.draw_card())
-					first_move = False
-					if self.isSplit and cards_dict[hand[0]] == "A":
-						break
-				elif move == "S":
-					break
-				elif move == "D" and first_move:
-					self.isDouble = True
-					self.money -= bet[0]
-					bet[0] *= 2
+                    self.playerCards.append([hand[1]])
+                    self.playerCards[0].pop()
 
-					hand.append(self.shoe.draw_card())
-					break
-				elif move == "P" and first_move and hand[0] == hand[1]:
-					self.isSplit = True
-					self.money -= bet[0]
-					bet += bet
+                    first_move = False
+                elif move == "X":
+                    self.money += 0.5 * bet[0]
+                    return -1
 
-					self.playerCards.append([hand[1]])
-					self.playerCards[0].pop()
+                self.print_hands(False)
 
-					first_move = False
-				elif move == "X":
-					self.money += 0.5*bet[0]
-					return -1
+                if self.sum_hands(hand)[0] > 21:
+                    if 1 == len(self.playerCards):
+                        return -1
+                    else:
+                        break
 
-				self.print_hands(False)
+        # Dealer Moves
+        player_sums = []
+        for i in range(len(self.playerCards)):
+            player_sums.append(self.sum_hands(self.playerCards[i]))
 
-				if self.sum_hands(hand) > 21:
-					if 1 == len(self.playerCards):
-						return -1
-					else:
-						break
+        while True:
+            self.print_hands()
+            dealer_sum, _ = self.sum_hands(self.dealerCards)
+            if 17 <= dealer_sum:
+                break
+            else:
+                self.dealerCards.append(self.shoe.draw_card())
 
-		#Dealer Moves
-		player_sums=[]
-		for i in range(len(self.playerCards)):
-			player_sums.append(self.sum_hands(self.playerCards[i]))
+        return self.manageBet(dealer_sum, player_sums, bet)
 
-		while True:
-			self.print_hands()
-			dealer_sum = self.sum_hands(self.dealerCards)
-			if 17 <= dealer_sum:
-				break
-			else:
-				self.dealerCards.append(self.shoe.draw_card())
+    def step(self, action):
+        hand = self.playerCards[self.currHand]
+        reward = 0
+        done = False
 
-		return self.manageBet(dealer_sum, player_sums, bet)
+        if action == "H":
+            hand.append(self.shoe.draw_card())
+        elif action == "D":
+            self.isDouble = True
+            hand.append(self.shoe.draw_card())
+        elif action == "P" and self.first_move and hand[0] == hand[1]:
+            self.isSplit = True
+            self.playerCards.append([hand[1]])
+            self.playerCards[0].pop()
+            self.playerCards[0].append(self.shoe.draw_card())
+            self.playerCards[1].append(self.shoe.draw_card())
 
-	def step(self, action): 
-		move = ""
-		hand = self.playerCards[self.splitHand]
-		reward = 0
-		done = False
+        elif (action == "P" and (not self.first_move or not hand[0] == hand[1])) or (action == "D" and not self.first_move):
+            reward = -100000000
+            done = True
+            player_state, game_state = self.sum_hands(hand)
+            dealer_state = self.dealerCards[0]
+            return game_state, player_state, dealer_state, reward, done
 
-		# if self.first_move:
-		# 	if self.playerCards[hand][0] == self.playerCards[hand][1]:
-		# 		move = input("Select your move: Hit - H, Stand - S, Double - D, Split - P, Surrender - X\n")
-		# 	else:
-		# 		move = input("Select your move: Hit - H, Stand - S, Double - D, Surrender - X\n")
-		# else:
-		# 	move = input("Select your move: Hit - H, Stand - S\n")
-		# move= move.upper()
+        elif action == "X":
+            reward = -0.5
+            done = True
 
-		if action == "H":
-			hand.append(self.shoe.draw_card())
-			# self.first_move = False
-		elif action == "D":
-			self.isDouble = True
-			# self.first_move = False
-			hand.append(self.shoe.draw_card())
-		elif action == "P" and self.first_move and hand[0] == hand[1]:
-			self.isSplit = True
-			self.playerCards.append([hand[1]])
-			self.playerCards[0].pop()
-			# self.first_move = False
-			self.playerCards[0].append(self.shoe.draw_card())
-			self.playerCards[1].append(self.shoe.draw_card())
+        sumHand, _ = self.sum_hands(hand)
+        if sumHand > 21:
+            if 1 == len(self.playerCards) and action == "D":
+                reward = -2
+                done = True
+            elif 1 == len(self.playerCards):
+                reward = -1
+                done = True
+            elif self.currHand == 0:
+                self.currHand = 1
+                hand = self.playerCards[self.currHand]
+            else:
+                reward, done = self.dealerMoves()
 
-		elif (action == "P" and (not self.first_move or not hand[0] == hand[1])):
-			reward = -20
-			done = True
-			player_state, game_state = self.sum_hands(hand)
-			dealer_state = self.dealerCards[0]
+        elif action == "S" or action == "D" or (action == "P" and self.isSplit):
+            reward, done = self.dealerMoves()
+            self.currHand = 1
 
-			return game_state, player_state, dealer_state, reward, done
+        self.first_move = False
+        player_state, game_state = self.sum_hands(hand)
+        dealer_state = self.dealerCards[0]
 
-		elif(action == "X"):
-			reward = -0.5
-			done = True
+        return game_state, player_state, dealer_state, reward, done
 
-		sumHand, _ = self.sum_hands(hand)
-		if (sumHand > 21):
-			if 1 == len(self.playerCards) and action == "D":
-				reward = -2
-				done = True
-			elif 1 == len(self.playerCards):
-				reward = -1
-				done = True
-			elif (self.splitHand == 0):
-				self.splitHand = 1
-				hand = self.playerCards[self.splitHand]
-			else:
-				reward, done = self.dealerMoves()
+    def dealerMoves(self):
+        # Dealer Moves
+        if self.currHand == 0 and self.isSplit:
+            return 0, False
+        player_sums = []
+        for i in range(len(self.playerCards)):
+            player_sum, _ = self.sum_hands(self.playerCards[i])
+            player_sums.append(player_sum)
 
-		elif (action == "S" or action == "D" or (action == "P" and self.isSplit)):
-			reward, done = self.dealerMoves()
-			self.splitHand = 1
+        while True:
+            # self.print_hands()
+            dealer_sum, _ = self.sum_hands(self.dealerCards)
+            if 17 <= dealer_sum:
+                break
+            else:
+                self.dealerCards.append(self.shoe.draw_card())
 
-		self.first_move = False
-		player_state, game_state = self.sum_hands(hand)
-		dealer_state = self.dealerCards[0]
-
-		return game_state, player_state, dealer_state, reward, done
-
-
-		
-
-	def dealerMoves(self):
-		#Dealer Moves
-		if(self.splitHand == 0 and self.isSplit):
-			return 0 , False
-		player_sums=[]
-		for i in range(len(self.playerCards)):
-			sum, _ = self.sum_hands(self.playerCards[i])
-			player_sums.append(sum)
-
-		while True:
-			# self.print_hands()
-			dealer_sum, _ = self.sum_hands(self.dealerCards)
-			if 17 <= dealer_sum:
-				break
-			else:
-				self.dealerCards.append(self.shoe.draw_card())
-
-		return self.rewardHandler(dealer_sum, player_sums), True
-
+        return self.rewardHandler(dealer_sum, player_sums), True
 
 # def main():
 # 	game = Game()
